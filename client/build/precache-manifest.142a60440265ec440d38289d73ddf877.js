5f13a4579a321f4800f40967dff8b8db18eded90
kingzmen <kingzmentechnology@gmail.com>
2020-01-31 05:32:44 +0100

    adding build

---
+++ client/build/precache-manifest.142a60440265ec440d38289d73ddf877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733464cca4ea5a316c233cb62647ee97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6929cb3b10136793c543",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "d4074852c85a7e8a26e9",
+    "url": "/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "6929cb3b10136793c543",
+    "url": "/static/js/2.f5ce4175.chunk.js"
+  },
+  {
+    "revision": "02acfcac3326d28f4b6283abdc2f1c64",
+    "url": "/static/js/2.f5ce4175.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4074852c85a7e8a26e9",
+    "url": "/static/js/main.71188da9.chunk.js"
+  },
+  {
+    "revision": "4742f480cddbcfa98bcd",
+    "url": "/static/js/runtime-main.f55b1089.js"
+  }
+]);
