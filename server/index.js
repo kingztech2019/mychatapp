387b8341d1f381e9f66b3273f12c62ca0a3d0d77
kingzmen <kingzmentechnology@gmail.com>
2020-01-31 04:32:04 +0100

    third commit

--- server/index.js
+++ server/index.js
@@ -92,8 +92,8 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     // index.html for all page routes
-    app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
     });
 }
 
